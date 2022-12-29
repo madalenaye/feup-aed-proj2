9b47b1602d281f13ea1b78bae054b6a25ec7b020
Madalena Ye <mads1040@gmail.com>
2022-12-29 00:38:49 +0000

    about to star implementing the statistics

--- classes/menu.cpp
+++ classes/menu.cpp
@@ -3,7 +3,7 @@
 //
 #include "Menu.h"
 
-//apresentação do menu
+//initial menu
 Menu::Menu() {
     printf("\n");
     printf("\033[44m===========================================================\033[0m\t\t");
@@ -11,24 +11,31 @@ Menu::Menu() {
     supervisor = new Supervisor();
 }
 
+//options of the menu
 void Menu::init() {
     int option;
     while(true){
-    cout << " O que deseja fazer hoje?\n" << "\n" << " [1] Realizar operação " << "\n" << " [2] Ver estatísticas" << "\n"<< " [3] Sair\n" << "\n Opção: ";
-    cin >> option;
-    switch (option) {
-        case 1 : //showOperations(); break;
-        case 2 : //showStatistics(); break;
-        case 3 : {end(); return;}
-        default: {
-            cout << "\n Input inválido, tente novamente\n\n";
-            cin.clear();
-            cin.ignore(INT_MAX, '\n');
-            break;
+        cout << " O que deseja fazer hoje?\n" << "\n" << " [1] Realizar operação " << "\n" << " [2] Ver estatísticas" << "\n"<< " [3] Sair\n" << "\n Opção: ";
+        cin >> option;
+        switch (option) {
+            case 1 : //showOperations(); break;
+            case 2 : showStatistics(); break;
+            case 3 : end(); return;
+            default: {
+                cout << "\n Input inválido, tente novamente\n\n";
+                cin.clear();
+                cin.ignore(INT_MAX, '\n');
+                break;
+            }
         }
     }
-    }
 }
+
+
+void Menu::showStatistics() {
+    cout << "\n Que estatísticas pretende analisar?";
+}
+//close menu
 void Menu::end() {
     printf("\n");
     printf("\033[46m===========================================================\033[0m\n");
