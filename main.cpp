3582e871733f2869f12b05d4880b3e14a8537afc
Du <luispedro.2003@hotmail.com>
2022-12-30 02:42:27 +0000

    testing...

--- main.cpp
+++ main.cpp
@@ -12,15 +12,27 @@ int main() {
     cout << "Target: ";
     cin >> dest;*/
 
+    cout << "Source airports: \n";
     double latitude, longitude, radius;
-    cout << "Latitude: ";
+    cout << " - Latitude: ";
     cin >> latitude;
 
-    cout << "Longitude: ";
+    cout << " - Longitude: ";
     cin >> longitude;
 
-    cout << "Radius: ";
+    cout << " - Radius: ";
     cin >> radius;
+
+    cout << "Target airports: \n";
+    double latitude1, longitude1, radius1;
+    cout << " - Latitude: ";
+    cin >> latitude1;
+
+    cout << " - Longitude: ";
+    cin >> longitude1;
+
+    cout << " - Radius: ";
+    cin >> radius1;
 
     auto map = supervisor.getMap();
     auto airports = supervisor.getAirports();
@@ -28,15 +40,54 @@ int main() {
     airlines.push_back("TAP");
     airlines.push_back("AAL");
     airlines.push_back("BAW");
+    airlines.push_back("EZY");
 
 
     //auto dist = graph.distance(airports.find(Airport(src))->getLatitude(),airports.find(Airport(src))->getLongitude(),airports.find(Airport(dest))->getLatitude(),airports.find(Airport(dest))->getLongitude());
     //cout << dist;
+    vector<string> source, dest;
 
-    cout << "Airports:\n";
+    for (auto a: airports) {
+        if (graph.distance(a.getLatitude(), a.getLongitude(), latitude, longitude) <= radius) {
+            source.push_back(a.getCode());
+        }
+    }
+    cout << "Source: ";
+    for (auto a : source){
+        cout << a << " - ";
+    }
+
+    cout << '\n';
+
     for (auto a: airports){
-        if (graph.distance(a.getLatitude(),a.getLongitude(),latitude,longitude) <= radius){
-            cout << a.getCode() << '\n';
+        if (graph.distance(a.getLatitude(),a.getLongitude(),latitude1,longitude1) <= radius1){
+            dest.push_back(a.getCode());
+        }
+    }
+    cout << "Target: ";
+    for (auto a : dest){
+        cout << a << " - ";
+    }
+
+    cout << '\n';
+
+
+    for (auto x : source){
+        for (auto y: dest){
+            if (x!=y){
+                cout << "Flight " << x << " to " << y << " : ";
+                auto vetor = graph.bfs(map[x],map[y],airlines);
+                if (vetor.second.empty()) cout << "Not found\n";
+
+                else {
+                    for (int i = 0; i < vetor.second.size() - 1; i++) {
+                        cout << vetor.second[i].getCode() << " --- ( ";
+                        cout << vetor.first[i] << " ) --> ";
+                    }
+                    cout << vetor.second[vetor.second.size() - 1].getName() << '\n';
+                    cout << "Number of flights : " << vetor.second.size() - 1 << '\n';
+                }
+            }
         }
     }
     /*
